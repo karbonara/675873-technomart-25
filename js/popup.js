15e63783d67c88142098a80fd9defd44d559a21f
karbonara <volkovartemka07@gmail.com>
2019-07-06 17:05:09 +0300

    Добавил JS

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,32 @@
-  var link = document.querySelector(".basket");
-  
-  var popup = document.querySelector(".modal-basket");
-  
-  link.addEventListener("click", function (evt) {
+ var mapLink = document.querySelector(".contacts-button-map");
+ var mapPopup = document.querySelector(".modal-map");
+ var mapClose = mapPopup.querySelector(".modal-close-map");
+ mapLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+ });
+ mapClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+ });
+ window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+       if (mapPopup.classList.contains("modal-show")) {
+          evt.preventDefault();
+          mapPopup.classList.remove("modal-show");
+       }
+    }
+ });
+ var link = document.querySelector(".btn-map");
+ var popup = document.querySelector(".modal-login");
+ var close = popup.querySelector(".modal-close");
+ var login = popup.querySelector("[user-name]");
+ link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-  });
+    login.focus();
+ });
+ close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+ });
